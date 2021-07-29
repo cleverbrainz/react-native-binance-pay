f679b1ceb1ce0d8efdda9f6a0fce3acf18cbdc4e
Tao Zhang <zhangtao820801@gmail.com>
2021-07-29 22:27:12 +0800

    basic functions
    init binance pay param and make payment

--- android/src/main/java/com/reactnativebinancepay/BinancePayModule.java
+++ android/src/main/java/com/reactnativebinancepay/BinancePayModule.java
@@ -2,18 +2,28 @@ package com.reactnativebinancepay;
 
 import androidx.annotation.NonNull;
 
+import com.binance.android.binancepay.api.BinancePay;
+import com.binance.android.binancepay.api.BinancePayException;
+import com.binance.android.binancepay.api.BinancePayFactory;
+import com.binance.android.binancepay.api.BinancePayListener;
+import com.binance.android.binancepay.api.BinancePayParam;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 @ReactModule(name = BinancePayModule.NAME)
 public class BinancePayModule extends ReactContextBaseJavaModule {
     public static final String NAME = "BinancePay";
 
+    BinancePay binancePay;
+    BinancePayParam binancePayParam;
+
     public BinancePayModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        binancePay = new BinancePayFactory.Companion().getBinancePay(reactContext);
     }
 
     @Override
@@ -22,6 +32,30 @@ public class BinancePayModule extends ReactContextBaseJavaModule {
         return NAME;
     }
 
+    @ReactMethod
+    public void initBinancePayParam(String merchantId, String prepayId, String timeStamp, String nonceStr, String certSn, String sign) {
+      binancePayParam = new BinancePayParam(merchantId, prepayId, timeStamp, nonceStr, certSn, sign);
+    }
+
+    @ReactMethod
+    public void makePayment(Promise promise) {
+      binancePay.pay(binancePayParam, new BinancePayListener() {
+        @Override
+        public void onSuccess() {
+          promise.resolve("success");
+        }
+
+        @Override
+        public void onCancel() {
+          promise.resolve("cancel");
+        }
+
+        @Override
+        public void onError(BinancePayException e) {
+          promise.resolve(e.getLocalizedMessage());
+        }
+      });
+    }
 
     // Example method
     // See https://reactnative.dev/docs/native-modules-android
